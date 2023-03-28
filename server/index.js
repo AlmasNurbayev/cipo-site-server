b8428d338e9e61f15d0b9a3355c989a23d28d7ae
Almas <ntldr@mail.ru>
2023-03-28 12:48:18 +0600

    add helmet and express-rate-limit

--- server/index.js
+++ server/index.js
@@ -8,10 +8,22 @@ import cors from 'cors';
 import fs from 'fs';
 import https from 'https';
 import http from 'http';
+import helmet from 'helmet';
+import rateLimit, { MemoryStore } from 'express-rate-limit'
 
 dotenv.config();
 
 const port = process.env.PORT_EXPRESS;
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	max: 50, // Limit each IP to  requests per `window`
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	store: new MemoryStore(),
+});
+
+// Apply the rate limiting middleware to all requests
+
+
 
 const app = express();
 app.use(cors());
@@ -19,7 +31,9 @@ app.use('/product_images',express.static('product_images'));
 app.use('/news_images',express.static('news_images'));
 app.use('/store_images',express.static('store_images'));
 app.use(express.json());
-app.use('/api', initRouterApi());
+app.use('/api', initRouterApi(), limiter);
+//app.use(limiter);
+app.use(helmet());
 
 let key = fs.readFileSync('./ssl/private.key');
 let cert = fs.readFileSync('./ssl/certificate.crt');
