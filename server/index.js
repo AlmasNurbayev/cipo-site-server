1660b09371fbb91f3dc0ba38e58f41b415e8ee7f
almas <ntldr@mail.ru>
2024-04-19 21:26:45 +0500

    add prom counter httpRequestCounter

--- server/index.js
+++ server/index.js
@@ -1,30 +1,36 @@
 'useStrict';
 
-import express from "express";
+import express from 'express';
 //import { logger } from "../utils/logger.js";
 import dotenv from 'dotenv';
-import {initRouterApi} from "./router.js";
+import { initRouterApi } from './router.js';
 import cors from 'cors';
 import fs from 'fs';
 import https from 'https';
 import http from 'http';
 import helmet from 'helmet';
-import rateLimit, { MemoryStore } from 'express-rate-limit'
-//import registerPromClient from 'prom/register.js'; 
-import client from 'prom-client'
+import rateLimit, { MemoryStore } from 'express-rate-limit';
+//import registerPromClient from 'prom/register.js';
+import client from 'prom-client';
 import { endTiming } from './prom/end_timing.js';
-import { startTiming } from "./prom/start_timing.js";
+import { startTiming } from './prom/start_timing.js';
+import { formatISO } from 'date-fns';
 
 dotenv.config();
+
+console.log(
+  formatISO(Date.now(), { representation: 'complete' }),
+  'init server...'
+);
 
 const portHTTP = process.env.PORT_HTTP;
 const portHTTPS = process.env.PORT_HTTPS;
 
 const limiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minutes
-	max: 50, // Limit each IP to  requests per `window`
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	store: new MemoryStore(),
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 50, // Limit each IP to  requests per `window`
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  store: new MemoryStore(),
 });
 const register = new client.Registry();
 client.collectDefaultMetrics({
@@ -34,27 +40,43 @@ client.collectDefaultMetrics({
   gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
   register,
 });
-export const http_request_duration_milliseconds  = new client.Histogram({
+export const http_request_duration_milliseconds = new client.Histogram({
   name: 'myapp_http_request_duration_milliseconds',
   help: 'Duration of HTTP requests in milliseconds',
   labelNames: ['method', 'route', 'statusCode', 'originalUrl'],
-  buckets: [1,10,50,100,200,500,1000],
+  buckets: [1, 10, 50, 100, 200, 500, 1000],
 });
-register.registerMetric(http_request_duration_milliseconds );
+register.registerMetric(http_request_duration_milliseconds);
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'statusCode', 'originalUrl'],
+});
+register.registerMetric(httpRequestCounter);
 
 const app = express();
 app.use(cors());
-app.use(startTiming);
-app.use( ( req, res, next ) => {
-  // для перехвата всех ответов в конце цепочки
-  res.on( 'finish', () => endTiming( req, res, next ) );
+app.use((req, res, next) => {
+  httpRequestCounter.inc({
+    method: req.method,
+    statusCode: res.statusCode,
+    route: req.path,
+    originalUrl: req.originalUrl,
+  });
   next();
 });
-// TODO: fix helmet, do not load images 
-app.use(helmet({
-  contentSecurityPolicy: false,
-  },
-));
+app.use(startTiming);
+app.use((req, res, next) => {
+  // для перехвата всех ответов в конце цепочки
+  res.on('finish', () => endTiming(req, res, next));
+  next();
+});
+// TODO: fix helmet, do not load images
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 // app.use('/product_images', express.static('product_images'));
 // app.use('/news_images', express.static('news_images'));
 // app.use('/store_images', express.static('store_images'));
@@ -74,23 +96,27 @@ const ca = fs.readFileSync('./ssl/ca_bundle.crt');
 const options = {
   key: key,
   cert: cert,
-  ca: ca
+  ca: ca,
 };
 
 const serverHTTP = http.createServer(app);
 const serverHTTPS = https.createServer(options, app);
 
 serverHTTPS.listen(portHTTPS, () => {
-   console.log(`server HTTPS starting on port: ${portHTTPS}`);
-   console.log(JSON.stringify(serverHTTPS.address()));
- });
-
- serverHTTP.listen(portHTTP, '0.0.0.0', () => {
-  console.log(`server HTTP for inside connections starting on port: ${portHTTP}`);
-  console.log(JSON.stringify(serverHTTP.address()));
+  console.log(
+    formatISO(Date.now(), { representation: 'complete' }),
+    `server HTTPS starting on port: ${portHTTPS}`
+  );
+  console.log(JSON.stringify(serverHTTPS.address()));
 });
 
- 
+serverHTTP.listen(portHTTP, '0.0.0.0', () => {
+  console.log(
+    formatISO(Date.now(), { representation: 'complete' }),
+    `server HTTP for inside connections starting on port: ${portHTTP}`
+  );
+  console.log(JSON.stringify(serverHTTP.address()));
+});
 
 // app.listen(port, ()=> {
 //     console.log('start express on port: ' + port);
